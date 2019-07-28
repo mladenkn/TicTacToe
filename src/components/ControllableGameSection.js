2d31e587dfdc0a78e9b256d91dcc7a84e001fd61
Mladen <mladsdf@sdf.com>
2019-07-28 14:15:29 +0200

    renamed to useTicTacToe and moved

--- src/components/ControllableGameSection.js
+++ src/components/ControllableGameSection.js
@@ -1,38 +1,15 @@
-import React, { useEffect, Fragment, } from 'react';
+import React, { Fragment, } from 'react';
 import GameSectionPresenter from './GameSection';
-import { playerMove } from '../redux/modules/ticTacToeRound';
-import { players } from '../ticTacToeConstants';
-import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-import { newRound, selectResultHistory } from '../redux/modules/ticTacToeRounds';
 import GameRestartDialogContent from './GameRestartDialog';
 import styled from 'styled-components';
 import { Dialog } from '@material-ui/core';
-import { isEmpty } from 'ramda';
-
-const useGameSectionLogic = (gameSize) => {
-
-  const state = useSelector(s => s.ticTacToeRounds);
-  const dispatch = useDispatch();
-  
-  const currentRound = state.current;
-  const roundInitialized = !isEmpty(currentRound);
-  const resultHistory = selectResultHistory(state.history);
-  const startNewRound = (gameSize) => dispatch(newRound(gameSize, players.x));
-  const onCellClick = ({row, col}) => dispatch(playerMove(row, col));
-
-  useEffect(() => {
-    if(!roundInitialized)
-      startNewRound(gameSize);
-  });
-
-  return { currentRound, roundInitialized, resultHistory, onStartNewRound: startNewRound, onCellClick }
-}
+import useTicTacToe from '../redux/connectors/useTicTacToe';
 
 export const ControllableGameSection = (({gameSize, onGoBack}) => {
 
   const { currentRound, roundInitialized, resultHistory, onStartNewRound, onCellClick } = 
-    useGameSectionLogic(gameSize);
+    useTicTacToe(gameSize);
 
   return roundInitialized ?
     <Fragment>
