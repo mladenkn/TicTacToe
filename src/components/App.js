52c8a364339066940ef3280160f406f44dee6ca1
Mladen <mladsdf@sdf.com>
2019-07-29 21:54:10 +0200

    moved contents of index.css to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,33 @@ import React from 'react';
 import HomePage from './HomePage';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import GamePage from './GamePage';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+
+  * {
+      box-sizing: border-box;
+  }
+`
 
 const App = () => (
   <Router>
-    <div>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/play/:gameSize" component={GamePage} />
-    </div>
+    <GlobalStyle />
+    <Route path="/" exact component={HomePage} />
+    <Route path="/play/:gameSize" component={GamePage} />
   </Router>
 )
 
